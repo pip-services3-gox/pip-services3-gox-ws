2c44ace6d5203acd28fa1a289a2b05e11e5b3b43
expproletariy <expproletariy@gmail.com>
2021-03-11 18:42:32 +0400

    copy repository from https://github.com/pip-services3-go/pip-services3-go-ws

---
+++ checkout.ps1
@@ -0,0 +1,8 @@
+#!/usr/bin/env pwsh
+
+git clone git@github.com:pip-services3-gox/pip-services3-commons-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-components-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-container-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-data-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-rpc-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-swagger-gox.git
