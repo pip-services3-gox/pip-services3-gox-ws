d7196560acf503d1db0f3847125e15ed666a3d19
Dmitry Krayniy <krdima92@gmail.com>
2022-05-10 16:10:44 +0300

    Added missing repos

--- checkout.ps1
+++ checkout.ps1
@@ -6,3 +6,6 @@ git clone git@github.com:pip-services3-gox/pip-services3-container-gox.git
 git clone git@github.com:pip-services3-gox/pip-services3-data-gox.git
 git clone git@github.com:pip-services3-gox/pip-services3-rpc-gox.git
 git clone git@github.com:pip-services3-gox/pip-services3-swagger-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-redis-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-grpc-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-memcached-gox.git
