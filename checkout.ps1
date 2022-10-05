dbcc77dc68a31494c3bde78ea6b43ab8e1eceaad
Levichev Dmitry <levichev.dmitry@gmail.com>
2022-10-06 00:12:03 +0400

    Update checkout.ps1

--- checkout.ps1
+++ checkout.ps1
@@ -1,15 +1,30 @@
 #!/usr/bin/env pwsh
 
 git clone git@github.com:pip-services3-gox/pip-services3-commons-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-expressions-gox.git
 git clone git@github.com:pip-services3-gox/pip-services3-components-gox.git
 git clone git@github.com:pip-services3-gox/pip-services3-container-gox.git
-git clone git@github.com:pip-services3-gox/pip-services3-data-gox.git
+
 git clone git@github.com:pip-services3-gox/pip-services3-rpc-gox.git
-git clone git@github.com:pip-services3-gox/pip-services3-swagger-gox.git
-git clone git@github.com:pip-services3-gox/pip-services3-redis-gox.git
 git clone git@github.com:pip-services3-gox/pip-services3-grpc-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-swagger-gox.git
+
+git clone git@github.com:pip-services3-gox/pip-services3-redis-gox.git
 git clone git@github.com:pip-services3-gox/pip-services3-memcached-gox.git
+
 git clone git@github.com:pip-services3-gox/pip-services3-messaging-gox.git
-git clone git@github.com:pip-services3-gox/pip-services3-expressions-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-mqtt-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-rabbitmq-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-kafka-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-nats-gox.git
+
+git clone git@github.com:pip-services3-gox/pip-services3-data-gox.git
 git clone git@github.com:pip-services3-gox/pip-services3-mongodb-gox.git
 git clone git@github.com:pip-services3-gox/pip-services3-postgres-gox.git
+
+git clone git@github.com:pip-services3-gox/pip-services3-azure-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-gcp-gox.git
+
+git clone git@github.com:pip-services3-gox/pip-services3-datadog-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-prometheus-gox.git
+git clone git@github.com:pip-services3-gox/pip-services3-elasticsearch-gox.git
